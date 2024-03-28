922ae13c2fb99d2ab1e4d7bae95d9f725775a45d
Jasmin Korat <jasminkorat204@gmail.com>
2024-03-28 18:21:58 +0530

    Manage appointment path in server side

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ require("./database/db")
 const express = require('express');
 const PORT = process.env.PORT
 const user = require("./routers/user")
+const appointment = require("./routers/appointment")
 
 const app = express();
 app.use(express.json());
 
 app.use("/user", user)
+app.use("/appointment", appointment)
 
 app.listen(PORT, () => {
       console.log(`Server running in http://localhost:${PORT}`);
