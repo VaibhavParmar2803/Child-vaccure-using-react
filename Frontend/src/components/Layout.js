8f4641e792642d87dd048829f68252b4cb14e034
Jasmin Korat <jasminkorat204@gmail.com>
2024-03-29 17:31:28 +0530

    Manage booking appointment in admin side

--- Frontend/src/components/Layout.js
+++ Frontend/src/components/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Toast } from 'primereact/toast';
 import AppSidebar from './AppSidebar';
 import { Icon } from '@iconify/react';
+import { useNavigate } from 'react-router-dom';
 
 const Layout = ({ children }) => {
+  const navigate = useNavigate();
+
   return (
     <div className='d-flex flex-column  overflow-hidden'>
       <div className='layout-header border-bottom'>
@@ -11,7 +13,8 @@ const Layout = ({ children }) => {
           CHILD VACCURE
         </div>
         <div className='logout'>
-          Logout <Icon icon="line-md:logout"  style={{color: 'black'}} />
+          <button className='appointment_button me-4' onClick={() => navigate('/book-appointment')}>Book Appointment</button>
+          Logout <Icon icon="line-md:logout" style={{ color: 'black' }} />
         </div>
       </div>
       <div>
